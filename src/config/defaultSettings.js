436820ffc55777af016b4163e7c58a451d72eaee
Ahmad Nazirul <zieru@ahmadnazirul.my.id>
2021-01-26 01:30:46 +0000

    little

--- src/config/defaultSettings.js
+++ src/config/defaultSettings.js
@@ -15,18 +15,18 @@
 
 export default {
   primaryColor: 'rgb(47, 84, 235)', // primary color of ant design
-  navTheme: 'dark', // theme for nav menu
-  layout: 'sidemenu', // nav menu position: sidemenu or topmenu
+  navTheme: 'light', // theme for nav menu
+  layout: 'topmenu', // nav menu position: sidemenu or topmenu
   contentWidth: 'Fluid', // layout of content: Fluid or Fixed, only works when layout is topmenu
   fixedHeader: true, // sticky header
-  fixSiderbar: true, // sticky siderbar
+  fixSiderbar: false, // sticky siderbar
   autoHideHeader: false, //  auto hide header
   colorWeak: false,
   multiTab: false,
   production: process.env.NODE_ENV === 'production' && process.env.VUE_APP_PREVIEW !== 'true',
   // vue-ls options
   storageOptions: {
-    namespace: 'r7s__', // key prefix
+    namespace: 'rxs__', // key prefix
     name: 'ls', // name variable Vue.[ls] or this.[$ls],
     storage: 'local' // storage name session, local, memory
   }
